3c2bf114fe054639ccd8b10177a61fe05626ecf9
Victor Pacheco <v.emanuel.pacheco@gmail.com>
2023-02-13 14:23:48 -0300

    Repair and Add services

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,11 +4,10 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: ['./hero-detail.component.css']
 })
-
 export class HeroDetailComponent {
 
-  @Input() hero?: Hero;
+  @Input() hero: Hero | undefined;
 
 }
